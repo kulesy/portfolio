03ff804893d7e00eee9ebeb3e5cd6964ef5c4441
kulesy <kulewidak@gmail.com>
2024-04-28 17:24:48 +1200

    fix endpoint

--- portfolio.client/src/consts/ApiConsts.js
+++ portfolio.client/src/consts/ApiConsts.js
@@ -1,6 +1,6 @@
 const Constants = {
   BaseUrl: process.env.REACT_APP_API_URL,
-  ImageDownloadApi: `${process.env.REACT_APP_API_URL}/caseimages/blob`,
+  ImageDownloadApi: `${process.env.REACT_APP_API_URL}/caseImages/blob`,
 };
 
 export default Constants;
